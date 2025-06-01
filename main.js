ddf6f52686d34cf3fe35dfd8d36bcdccccd1863b
xamon <ramadanff991@gmail.com>
2025-06-02 00:51:58 +0700

    first commit

--- main.js
+++ main.js
@@ -18,3 +18,17 @@
     const content = document.getElementById("accordionContent");
     content.style.maxHeight = "0px"; // collapsed by default
   });
+  
+  
+  function showSection(id) {
+    const sections = document.querySelectorAll("section");
+    sections.forEach(section => {
+      section.classList.add("hidden");
+    });
+
+    const target = document.getElementById(id);
+    if (target) {
+      target.classList.remove("hidden");
+      window.scrollTo({ top: 0, behavior: 'smooth' }); // scroll ke atas jika ingin
+    }
+  }
